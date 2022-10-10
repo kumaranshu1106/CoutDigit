34a8dc791a5447de9fa6d5d88f4653cb76852fd6
kumaranshu1106 <114850574+kumaranshu1106@users.noreply.github.com>
2022-10-10 08:44:14 +0530

    Create countDigit.java

---
+++ countDigit.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AllDivisible {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n = sc.nextInt();
+System.out.println(i+"");
+            }
+            i++;
+        }
+    }
+}
