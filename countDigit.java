7d9171bf0b7b514eeea21b8c4b7e57d952c7ecd6
Sushantfox <115439873+Sushantfox@users.noreply.github.com>
2022-10-10 08:51:26 +0530

    Update countDigit.java

--- countDigit.java
+++ countDigit.java
@@ -4,6 +4,9 @@ public class AllDivisible {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number:");
         int n = sc.nextInt();
+        int i=1;
+        while(i<=n){
+            if(n%i==0){
 System.out.println(i+"");
             }
             i++;
